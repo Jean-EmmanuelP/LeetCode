b80a27441db38c55ec3a61e0968088f98c91eee7
Jean-Emmanuel Perramant <perramant.work@gmail.com>
2023-01-21 14:37:52 +0100

    new commit

--- Array/Two_sums/C/two_sums.c
+++ Array/Two_sums/C/two_sums.c
@@ -27,7 +27,7 @@ int main(void)
     int tab[4] = {1, 2, 3, 4};
     int target = 3;
     int *returnSize;
-    returnSize = malloc(sizeof(int *))
+    returnSize = malloc(sizeof(int) * 1)
     int *result = twoSum(tab, 4, target, returnSize);
     printf("Indices of the numbers that add up to the target value: %d", result);
     return (0);
