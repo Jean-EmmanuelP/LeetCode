78fb60efd24252c054f82003e9c385ecdcc00b22
Jean-Emmanuel Perramant <perramant.work@gmail.com>
2023-01-21 14:16:15 +0100

    new commit

--- Array/Two_sums/two_sums.c
+++ Array/Two_sums/two_sums.c
@@ -1,2 +1,23 @@
 /* link to the problem : https://leetcode.com/problems/two-sum/ */
 
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    *returnSize = 2;
+    int *out = malloc(sizeof(int) * 2);
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i+1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                out[0] = i;
+                out[1] = j;
+                return (out);
+            }
+        }
+    }
+    return (out);
+}
