913065633242315c51c74e544e48dbaf62093294
Jean-Emmanuel Perramant <perramant.work@gmail.com>
2023-01-21 14:20:19 +0100

    new commit

--- Array/Two_sums/C++/two_sums.cpp
+++ Array/Two_sums/C++/two_sums.cpp
@@ -7,7 +7,7 @@ class Solution
             int n=nums.size();
             for (int i = 0; i< n; i++)
             {
-                for (int j = i+!)
+                for (int j = i+1; j < n;)
             }
         }
 }
