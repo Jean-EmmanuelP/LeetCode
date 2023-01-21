0e439cc9a1c74a16b7390f0c17a9566b8da98024
Jean-Emmanuel Perramant <perramant.work@gmail.com>
2023-01-21 14:20:52 +0100

    new commit

--- Array/Two_sums/C++/two_sums.cpp
+++ Array/Two_sums/C++/two_sums.cpp
@@ -11,7 +11,8 @@ class Solution
                 {
                     if (nums[i] + nums[j] == target)
                     {
-                        vect.push_back()
+                        vect.push_back(i);
+                        
                     }
                 }
             }
