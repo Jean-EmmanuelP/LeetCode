7818d58b4dec4ed8e872ccc845279202bb43d3ca
Jean-Emmanuel Perramant <perramant.work@gmail.com>
2023-01-21 14:34:49 +0100

    new commit

--- Array/Two_sums/C/two_sums.c
+++ Array/Two_sums/C/two_sums.c
@@ -26,7 +26,7 @@ int main(void)
     int target = 3;
     int *returnSize;
     int *result = twoSums(tab, 4, target, returnSize);
-    printf("Indices of the numbers that add up to the target value")
+    printf("Indices of the numbers that add up to the target value: %d", result);
 }
 
 #endif
