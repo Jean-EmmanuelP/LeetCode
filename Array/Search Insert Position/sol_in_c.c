bf3db300357a164d9281af2239b2f8c9314932b6
Jean-emmanuel PERRAMANT <jperrama@e1r12p19.clusters.42paris.fr>
2023-01-22 21:26:38 +0100

    new commit

--- Array/Search Insert Position/sol_in_c.c
+++ Array/Search Insert Position/sol_in_c.c
@@ -3,9 +3,14 @@
 /* Source to understand some notion :
 https://stackoverflow.com/questions/2307283/what-does-olog-n-mean-exactly
 */
-/*
 
+
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
 */
+
 int searchInsert(int* nums, int numsSize, int target){
     for (int i = 0; i <= numsSize - 2; i++)
     {
