1059ab6ef6322bfc26062fa58c279e23815c317d
Jean-emmanuel PERRAMANT <jperrama@e1r12p19.clusters.42paris.fr>
2023-01-22 21:26:32 +0100

    new commit

--- Array/Search Insert Position/sol_in_c.c
+++ Array/Search Insert Position/sol_in_c.c
@@ -3,4 +3,16 @@
 /* Source to understand some notion :
 https://stackoverflow.com/questions/2307283/what-does-olog-n-mean-exactly
 */
+/*
 
+*/
+int searchInsert(int* nums, int numsSize, int target){
+    for (int i = 0; i <= numsSize - 2; i++)
+    {
+        if (nums[i] == target)
+            return (i);
+        else if (nums[i] + 1 == target)
+            return (i + 1);
+    }
+    return (numsSize);
+}
