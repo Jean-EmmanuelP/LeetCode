7cd6271b63f9f3ffb13f4be4a87e5092c03f0b88
Jean-emmanuel PERRAMANT <jperrama@e1r12p19.clusters.42paris.fr>
2023-01-22 21:11:17 +0100

    new commit

--- Array/remove-element/sol_in_c.c
+++ Array/remove-element/sol_in_c.c
@@ -1,3 +1,21 @@
 /*
 https://leetcode.com/problems/remove-element/submissions/883277421/
 */
+
+int removeElement(int* nums, int numsSize, int val){
+    int r = 0, w = 0;
+    while (r < numsSize)
+    {
+        if (nums[r] == val)
+        {
+            r++;
+        }
+        else
+        {
+            nums[w] = nums[r];
+            w++;
+            r++;
+        }
+    }
+    return (w);
+}
