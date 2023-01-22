9c5982343c14b3b307db2701153b12248c075a2a
Jean-emmanuel PERRAMANT <jperrama@e1r12p19.clusters.42paris.fr>
2023-01-22 21:26:22 +0100

    new commit

--- Array/Search Insert Position/sol_in_c.c
+++ Array/Search Insert Position/sol_in_c.c
@@ -1,3 +1,6 @@
 /* https://leetcode.com/problems/search-insert-position/ */
 
-/* */
+/* Source to understand some notion :
+https://stackoverflow.com/questions/2307283/what-does-olog-n-mean-exactly
+*/
+
