046bbcd691de405e2080223f0a3d3f6b3b31360a
Jean-emmanuel PERRAMANT <jperrama@e2r7p19.clusters.42paris.fr>
2023-01-24 22:44:15 +0100

    new commit

--- test.c
+++ test.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <limits.h>
 
+void ft_putstr(struct {a})
+{
+    printf("%d\n", a);
+}
+
 int main(void)
 {
-    unsigned int    max;
+    unsigned int  max;
 
     max = -1;
     printf("%d\n", max);
     printf("%lu\n", sizeof(int));
+    ft_putstr({42});
 }
+// 1000 0000
